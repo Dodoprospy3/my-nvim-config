17de0fec7658e3e63b1fa1dfecfda4b8b59c7807
Dodoprospy3 <dodoprospy3@gmail.com>
2026-08-14 19:08:23 +0300

    added refactoring.lua

---
+++ lua/plugins/refactoring.lua
@@ -0,0 +1,19 @@
+return {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+        require("refactoring").setup()
+
+        vim.keymap.set(
+            { "n", "x" },
+            "<leader>rr",
+            function()
+                require("refactoring").select_refactor()
+            end,
+            { desc = "Refactoring menu" }
+        )
+    end,
+}
