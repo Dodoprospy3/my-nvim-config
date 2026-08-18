7e04b67eb2a1bb9866b6d159d9da848620609a32
Dodoprospy3 <dodoprospy3@gmail.com>
2026-08-18 08:19:58 +0300

    fixed harpoon API issues

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,46 +4,75 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
+
     config = function()
         local harpoon = require("harpoon")
 
-        harpoon:setup()
+        -- Required Harpoon 2 setup
+        harpoon:setup({
+            settings = {
+                save_on_toggle = true,
+                sync_on_ui_close = true,
+            },
+        })
 
-        -- Add/remove current file
+        -- Add current file
         vim.keymap.set("n", "<leader>a", function()
-            local list = harpoon:list()
-            local current_file = vim.api.nvim_buf_get_name(0)
-
-            for i, item in ipairs(list.items) do
-                if item.value == current_file then
-                    list:remove_at(i)
-                    return
-                end
-            end
-
-            list:add()
-        end, { desc = "Harpoon: Toggle file" })
+            harpoon:list():add()
+        end, {
+            desc = "Harpoon: Add file",
+        })
 
         -- Open Harpoon menu
         vim.keymap.set("n", "<C-e>", function()
             harpoon.ui:toggle_quick_menu(harpoon:list())
-        end, { desc = "Harpoon: Menu" })
+        end, {
+            desc = "Harpoon: Menu",
+        })
 
-        -- Select files
-        vim.keymap.set("n", "<C-1>", function()
+        -- Select Harpoon files
+        vim.keymap.set("n", "<leader>1", function()
             harpoon:list():select(1)
-        end, { desc = "Harpoon: File 1" })
+        end, {
+            desc = "Harpoon: File 1",
+        })
 
-        vim.keymap.set("n", "<C-2>", function()
+        vim.keymap.set("n", "<leader>2", function()
             harpoon:list():select(2)
-        end, { desc = "Harpoon: File 2" })
+        end, {
+            desc = "Harpoon: File 2",
+        })
 
-        vim.keymap.set("n", "<C-3>", function()
+        vim.keymap.set("n", "<leader>3", function()
             harpoon:list():select(3)
-        end, { desc = "Harpoon: File 3" })
+        end, {
+            desc = "Harpoon: File 3",
+        })
 
-        vim.keymap.set("n", "<C-4>", function()
+        vim.keymap.set("n", "<leader>4", function()
             harpoon:list():select(4)
-        end, { desc = "Harpoon: File 4" })
+        end, {
+            desc = "Harpoon: File 4",
+        })
+
+        -- Previous / next Harpoon file
+        vim.keymap.set("n", "<leader>[", function()
+            harpoon:list():prev()
+        end, {
+            desc = "Harpoon: Previous",
+        })
+
+        vim.keymap.set("n", "<leader>]", function()
+            harpoon:list():next()
+        end, {
+            desc = "Harpoon: Next",
+        })
+
+        -- Remove current file from Harpoon
+        vim.keymap.set("n", "<leader>d", function()
+            harpoon:list():remove()
+        end, {
+            desc = "Harpoon: Remove file",
+        })
     end,
 }
