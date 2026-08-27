6d3ddcb9cb98b3822e023f666622741e622d346e
Dodoprospy3 <dodoprospy3@gmail.com>
2026-08-27 13:42:54 +0300

    fix

--- init.lua
+++ init.lua
@@ -4,5 +4,17 @@ require("config.lazy")
 require("config.lsp")
 require("config.transparency")
 vim.cmd.colorscheme("rose-pine")
+vim.opt.cursorline = false
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "Visual", {
+    fg = "#ffffff",
+    bg = "#666666",
+})
 vim.opt.shortmess:append("I")
 vim.keymap.set("n", "<F5>", ":!python %<CR>", { desc = "Run Python file" })
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.wrap = false
+vim.opt.signcolumn = "yes"
